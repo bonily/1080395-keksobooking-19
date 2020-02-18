1ab136a63103bb7bf56616c7119f0e8f1202b7db
bonily <bonilya@gmail.com>
2020-02-19 01:57:25 +0300

    Загрузка данных

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@
 
 (function () {
 
-  var ads = window.data.create();
+  var ads = [];
+
+  function onError() {
+    // console.error(message);
+  }
+
+  function onSuccess(data) {
+    ads = data;
+  }
 
   function activatePage() {
     window.map.activate();
@@ -20,5 +28,6 @@
   window.form.setAddress(window.map.getCoords());
   window.map.setMainPinClick(activatePage);
   window.map.moveMainPin(setNewAddress);
+  window.request('https://js.dump.academy/keksobooking/data', onSuccess, onError);
 
 })();
---
+++ js/request.js
@@ -0,0 +1,36 @@
+'use strict';
+(function () {
+  var errors = {
+    400: 'Неверный запрос',
+    401: 'Пользователь не авторизован',
+    404: 'Ничего не найдено'
+  };
+
+  function getRequest(url, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + errors[xhr.status]);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    xhr.open('GET', url);
+    xhr.send();
+  }
+  window.request = getRequest;
+})();
